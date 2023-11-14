4094ec1aab943ed48259deb45fe9768a4d934a62
Hala Azmi <68159055+xxHala@users.noreply.github.com>
2023-11-14 19:43:36 +0300

    Add files via upload

---
+++ shopping/src/components/product/index.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Grid from '@/components/grid';
+import Head from 'next/head'; // Import the Head component
+import styles from './product.module.scss';
+
+const Product = ({ product }) => {
+  if (product.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const pageTitle = 'ProductList';
+  const pageDescription = 'A list of Some Of Our Products';
+
+  return (
+    <div className={styles.g}>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+      </Head>
+      <Grid data={product} />
+    </div>
+  );
+};
+
+export default Product;
